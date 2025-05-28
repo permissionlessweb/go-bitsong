f24fee968b0dac7a29fb6f2fbf0e9a805ec155de
hard-nett <hardnettt@proton.me>
2025-05-28 06:39:19 -0400

    wip: add bls library (from ethereum), and bls12_381 authenticator

---
+++ crypto/bls/blst/init.go
@@ -0,0 +1,27 @@
+//go:build ((linux && amd64) || (linux && arm64) || (darwin && amd64) || (darwin && arm64) || (windows && amd64)) && !blst_disabled
+
+package blst
+
+import (
+	"fmt"
+	"runtime"
+
+	"github.com/bitsongofficial/go-bitsong/btsgutils/cache/nonblocking"
+	"github.com/bitsongofficial/go-bitsong/crypto/bls/common"
+	blst "github.com/supranational/blst/bindings/go"
+)
+
+func init() {
+	// Reserve 1 core for general application work
+	maxProcs := runtime.GOMAXPROCS(0) - 1
+	if maxProcs <= 0 {
+		maxProcs = 1
+	}
+	blst.SetMaxProcs(maxProcs)
+	onEvict := func(_ [48]byte, _ common.PublicKey) {}
+	keysCache, err := nonblocking.NewLRU(maxKeys, onEvict)
+	if err != nil {
+		panic(fmt.Sprintf("Could not initiate public keys cache: %v", err))
+	}
+	pubkeyCache = keysCache
+}
